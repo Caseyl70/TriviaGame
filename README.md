fa1a3baea09c9fa48e7839f8bbf9c2dffaeaf122
Casey Levin <clevin8080@yahoo.com>
2019-11-14 20:18:05 -0700

    Styling nd Functionality of the game

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ Welcome To the Office Trivia Test your Knowledge in this fun Interactive Trivia 
 
 In this game you will have options to pick the correct answer failure to answer you 
 will recieve a message from dwight. There is a timer for each question so that way you cant go and find the answer. If you get it right you will proceed throughout the Game Until the final question!
+
+
+User hits start button start button will disapear 
+    - Populate first question.
+     - question with choices as radio buttons
+     - Once user submits answer to the  question in alotted time should indicate a correct or inccorrect alert 
+     - move on to next question
+     After answering questions should give total of wins vs losses
+     After Game is over reset button should populate to start a new game.
+     
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,201 +1,191 @@
 // Inserting Questions
+var card = $("#officeTrivia");
+var countStartNumber = 20;
 
 var office = [
-    // Question 1
-    {
-        officeQuestion: "Season One Episode One, Who is the young padawon Michael is talking too?",
-        officeOptions: ["Pam", "Darrel", "Jim", "Kevin"],
-        officeAns: 2
-    },
+  // Question 1
+  {
+    officeQuestion: "Season One Episode One, Who is the young padawon randomIndex is talking too?",
+    officeOptions: ["Stanley", "Darrel", "Jim", "Kevin"],
+    officeAns: "Jim",
+    image: "assets/images/200w.webp"
+  },
 
-    // Question 2
-    {
-        officeQuestion: " How much did it cost Jim to Impersonate Dwight?",
-        officeOptions: ["20 Dollars", "15 Dollars", "34 Dollars", "11 Dollars", "7 Dollars"],
-        officeAns: 3
-    },
+  // Question 2
+  {
+    officeQuestion: " How much did it cost Jim to Impersonate Dwight?",
+    officeOptions: ["20 Dollars", "15 Dollars", "34 Dollars", "11 Dollars", "7 Dollars"],
+    officeAns: "11 Dollars",
+    image: "assets/images/200.webp"
+  },
 
-    // Question 3
-    {
-        officeQuestion: "Who is Jim trying to Beat in a Ping-Pong Match?",
-        officeOptions: ["Dwight", "Stanley", "Ryan", "Darrel"],
-        officeAns: 3
-    },
+  // Question 3
+  {
+    officeQuestion: "Who is Jim trying to Beat in a Ping-Pong Match?",
+    officeOptions: ["Dwight", "Stanley", "Ryan", "Darrel"],
+    officeAns: "Darrel",
+    image: "assets/images/giphy (3).webp"
+  },
 
-    // Question 4
-    {
-        officeQuestion: "What does Dwight Save Jim From Roy With?",
-        officeOptions: ["Pepper-Spray", "Throwing Star", "Gun", "His Karate Skills"],
-        officeAns: 0
-    },
+  // Question 4
+  {
+    officeQuestion: "What does Dwight Save Jim From Roy With?",
+    officeOptions: ["Pepper-Spray", "Throwing Star", "Gun", "His Karate Skills"],
+    officeAns: "Pepper-Spray",
+    image: "assets/images/giphy.webp"
+  },
 
-    // Question 5
-    {
-        officeQuestion: " Where are the Dundies Hosted Each Year?",
-        officeOptions: ["AppleBee's", "Chili's", "Benihana", "The Office"],
-        officeAns: 1
-    },
+  // Question 5
+  {
+    officeQuestion: " Where are the Dundies Hosted Each Year?",
+    officeOptions: ["AppleBee's", "Chili's", "Benihana", "The Office"],
+    officeAns: "Chili's",
+    image: "assets/images/chilis.webp"
+  },
 
-    // Question 6
-    {
-        officeQuestion: "Where Did Jim and Pam Get Married?",
-        officeOptions: ["Niagara Falls", "The Office", "Steam Town Mall", "The Warehouse"],
-        officeAns: 0
-    },
+  // Question 6
+  {
+    officeQuestion: "Where Did Jim and Pam Get Married?",
+    officeOptions: ["Niagara Falls", "The Office", "Steam Town Mall", "The Warehouse"],
+    officeAns: "Niagara Falls",
+    image: "https://media1.giphy.com/media/l0MYt5jPR6QX5pnqM/200.webp?cid=790b76118d2333ca52f8bbd7cff790d1f4e0e1d80e4a7118&rid=200.webp"
+  },
 
-    // Question 7
-    {
-        officeQuestion: "How Far Away Was Scranton from Utica in the Weight loss Challange?",
-        officeOptions: ["10lbs", "2lbs", "5lbs", "8lbs", "3lbs", "9lbs"],
-        officeAns: 3
-    },
+  // Question 7
+  {
+    officeQuestion: "How Far Away Was Scranton from Utica in the Weight loss Challange?",
+    officeOptions: ["10lbs", "2lbs", "5lbs", "8lbs", "3lbs", "9lbs"],
+    officeAns: "8lbs",
+    image: "assets/images/200 (1).webp"
+  },
 
-    // Question 8
-    {
-        officeQuestion: "Who Was Dunder Miflin Bought Out By?",
-        officeOptions: ["Michael Scott Paper Company", "David Wallace", "Saber", "Athlead"],
-        officeAns: 2
-    },
+  // Question 8
+  {
+    officeQuestion: "Who Was Dunder Miflin Bought Out By?",
+    officeOptions: ["Micheal Scott Paper Company", "David Wallace", "Sabre", "Athlead"],
+    officeAns: "Sabre",
+    image: "assets/images/giphy (4).webp"
+  },
 
-    // Question 9
-    {
-        officeQuestion: "Why did Jim and Pam Get Divorced?",
-        officeOptions: ["Jim Cheated", "Athlead", "Pam Cheated", "They Didnt", "They were never actually married", "Jam Forever"],
-        officeAns: 3
-    },
+  // Question 9
+  {
+    officeQuestion: "Why did Jim and Pam Get Divorced?",
+    officeOptions: ["Jim Cheated", "Athlead", "userInput Cheated", "They Didnt", "They were never actually married", "Jam Forever"],
+    officeAns: "They Didnt",
+    image: "assets/images/giphy (2).webp"
+  },
 
-    // Question 10
-    {
-        officeQuestion: "What is Jim role in Dwights Wedding?",
-        officeOptions: ["Best Man", "His Own Enemy", "Bestest Mench", "Idiot"],
-        officeAns: 2
-    },
+  // Question 10
+  {
+    officeQuestion: "What is Jim role in Dwights Wedding?",
+    officeOptions: ["Best Man", "His Own Enemy", "Bestest Mench", "losses"],
+    officeAns: "Bestest Mench",
+    image: "assets/images/giphy (1).webp"
+  },
 ];
-//Variables 
-var theOfficeCount = office.length;
-var intervalId;
-// User answers.
-var Uncanny = 0;
-var Idiot = 0;
-var season10 = 0;
-// Timer
-var timer = 20;
-var timerCount = false;
-var shruteSurvivalGuide = "";
-var dwight = [];
-var pam;
-var michael;
-//end variables
+var counter;
+var theOfficeTriviaGame = {
+  office: office,
+  currentQuestion: 0,
+  counter: countStartNumber,
+  uncanny: 0,
+  idiot: 0,
 
-
-// Answers and questions 
-$(document).ready(function() {
-    $(".start").on("click", function() {
-            $(".start").hide();
-            officeQuestion();
-            stanleysHeart();
-            //for loop to push the questions through when the user wants to play
-            for (var i = 0; i < dwight.length; i++) {
-                office.push(dwight[i]);
-            }
-        })
-        //Reset Function
-    $(".reset").on("click", function() {
-            $(".reset").hide();
-            $(".answer").dwight();
-            $(".question").dwight();
-            //for loop for reset function
-            for (var i = 0; i < dwight.length; i++) {
-                office.push(dwight[i]);
-            }
-            Uncanny = 0;
-            Idiot = 0;
-            season10 = 0;
-            timerCount = false;
-            officeQuestion();
-            stanleysHeart();
-        }) // start timer functionality
-
-    function stanleysHeart() {
-        if (timerCount) {
-            intervalId = setInterval(down, 1000);
-            timerCount = true;
-        }
-    }; //count down functionality for the timer.
-    function down() {
-        $(".time").html("<h1>Confrence Room In:" + timer + "</h1>");
-        timer--;
-        // if Statemment to turn timer count down to 0 and give the user an unanswerd question or a season10 since it doesnt exist.
-        if (timer === 0) {
-            season10++;
-            stop();
-            $(".answer").html("<h1> That's What She Said:" + pam.officeOptions[pam.officeAns] + "</h1>");
-            officeQuestion();
-            timer = 20;
-            stanleysHeart();
-        }
-
-    };
-    // This function will stop the timer.
-    function stop() {
-        clearInterval(intervalId);
-        timerCount = false;
-    };
-    // this function will select a qusetion at random so user cannot redo and get the answer correct.
-    function officeQuestion() {
-        michael = Math.floor(Math.random() * office.length);
-        pam = office[officeQuestion];
-        //  inserting if else statement to give user an uncanny or an idiot talley
-        if (pam) {
-            results();
-        } else {
-            $(".question").html("<h1>" + michael.officeQuestion + "</h1>");
-            // user will loop through this data upon answering the question
-            for (var i = 0; i < pam.officeOptions.length; i++) {
-                var officeKnowItAll = $("<div>");
-                officeKnowItAll.addClass("officeSelection");
-                officeKnowItAll.html(pam.officeOptions[i]);
-                officeKnowItAll.attr("user-value", i);
-                $(".question").append(officeKnowItAll);
-            };
-        }
-    };
-
-    // officeKnowItAll Selection functionality.
-    $(".question").on("click", ".officeSelection", function() {
-        shruteSurvivalGuide = parseInt($(this).attr("user-value"));
-        // if else statment for when the user either gets the question right or if the user gets the question wrong or uncanny nor Idiot
-        if (shruteSurvivalGuide === pam.officeAns) {
-            stop();
-            Uncanny++;
-            shruteSurvivalGuide = "";
-            $(".answer").html("<h3>Uncanny</h3>");
-            office.splice(michael, 1);
-            timer = 20;
-            stanleysHeart();
-            officeQuestion();
-        } else {
-            stop();
-            Idiot++;
-            shruteSurvivalGuide = "";
-            $(".answer").html("<h3>Idiot</h3>");
-            office.splice(michael, 1);
-            timer = 20;
-            stanleysHeart();
-            officeQuestion();
-        }
-    });
-    // ENDGAME...
-    function results() {
-        stop();
-        $(".time").dwight();
-        $(".question").dwight();
-        $(".question").html("<h3>Season 9 Episode 23 </h3>");
-        $(".answer").dwight();
-        $(".answer").append("<h3>Uncanny:" + Uncanny + "</h3>");
-        $(".answer").append("<h3>Idiot:" + Idiot + "</h3>");
-        $(".answer").append("<h3>Ignorant Slut:" + season10 + "</h3>");
-        $(".reset").show();
-
+  down: function () {
+    theOfficeTriviaGame.counter--;
+    $("#counter-number").text(theOfficeTriviaGame.counter);
+    if (theOfficeTriviaGame.counter === 0) {
+      console.log("TIME UP");
+      theOfficeTriviaGame.timeUp();
     }
+  },
+  dundie: function () {
+    counter = setInterval(theOfficeTriviaGame.down, 1000);
+    card.html("<h2>" + office[this.currentQuestion].officeQuestion + "</h2>");
+    for (var i = 0; i < office[this.currentQuestion].officeOptions.length; i++) {
+      card.append("<button class='answer-button' id='button' data-name='" + office[this.currentQuestion].officeOptions[i]
+        + "'>" + office[this.currentQuestion].officeOptions[i] + "</button>");
+    }
+  },
+
+  nextQuestion: function () {
+    theOfficeTriviaGame.counter = countStartNumber;
+    $("#counter-number").text(theOfficeTriviaGame.counter);
+    theOfficeTriviaGame.currentQuestion++;
+    theOfficeTriviaGame.dundie();
+  },
+  timeUp: function () {
+    clearInterval(counter);
+    $("#counter-number").html(theOfficeTriviaGame.counter);
+    card.html("<h2>Out of Time!</h2>");
+    card.append("<h3>The Correct Answer was: " + office[this.currentQuestion].officeAns);
+    card.append("<img src='" + office[this.currentQuestion].image + "' />");
+    if (theOfficeTriviaGame.currentQuestion === office.length - 1) {
+      setTimeout(theOfficeTriviaGame.results, 3 * 1000);
+    }
+    else {
+      setTimeout(theOfficeTriviaGame.nextQuestion, 3 * 1000);
+    }
+  },
+
+  results: function () {
+    clearInterval(counter);
+    card.html("<h2>All done, heres how you did!</h2>");
+    $("#counter-number").text(theOfficeTriviaGame.counter);
+    card.append("<h3>Correct Answers: " + theOfficeTriviaGame.uncanny + "</h3>");
+    card.append("<h3>Incorrect Answers: " + theOfficeTriviaGame.idiot + "</h3>");
+    card.append("<h3>Unanswered: " + (office.length - (theOfficeTriviaGame.idiot + theOfficeTriviaGame.uncanny)) + "</h3>");
+    card.append("<br><button id='start-over'>Start Over?</button>");
+  },
+  clicked: function (e) {
+    clearInterval(counter);
+    if ($(e.target).attr("data-name") === office[this.currentQuestion].officeAns) {
+      this.answeredCorrectly();
+    }
+    else {
+      this.answeredIncorrectly();
+    }
+  },
+  answeredIncorrectly: function () {
+    theOfficeTriviaGame.idiot++;
+    clearInterval(counter);
+    card.html("<h2>Nope!</h2>");
+    card.append("<h3>The Correct Answer was: " + office[theOfficeTriviaGame.currentQuestion].officeAns + "</h3>");
+    card.append("<img src='" + office[theOfficeTriviaGame.currentQuestion].image + "' />");
+    if (theOfficeTriviaGame.currentQuestion === office.length - 1) {
+      setTimeout(theOfficeTriviaGame.results, 3 * 1000);
+    }
+    else {
+      setTimeout(theOfficeTriviaGame.nextQuestion, 3 * 1000);
+    }
+  },
+  answeredCorrectly: function () {
+    clearInterval(counter);
+    theOfficeTriviaGame.uncanny++;
+    card.html("<h2>Correct!</h2>");
+    card.append("<img src='" + office[theOfficeTriviaGame.currentQuestion].image + "' />");
+    if (theOfficeTriviaGame.currentQuestion === office.length - 1) {
+      setTimeout(theOfficeTriviaGame.results, 3 * 1000);
+    }
+    else {
+      setTimeout(theOfficeTriviaGame.nextQuestion, 3 * 1000);
+    }
+  },
+  reset: function () {
+    this.currentQuestion = 0;
+    this.counter = countStartNumber;
+    this.uncanny = 0;
+    this.incorrect = 0;
+    this.dundie();
+  }
+};
+$(document).on("click", "#start-over", function () {
+  theOfficeTriviaGame.reset();
+});
+$(document).on("click", ".answer-button", function (e) {
+  theOfficeTriviaGame.clicked(e);
+});
+$(document).on("click", "#start", function () {
+  $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>30</span> Seconds</h2>");
+  theOfficeTriviaGame.dundie();
 });
